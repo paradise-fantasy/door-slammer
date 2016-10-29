60ce6d2dc69e23a4f012766bea875007034cda2b
khrall <raymitoro@gmail.com>
2016-10-29 16:22:13 +0200

    Trying to strip down code

--- sensortag.js
+++ sensortag.js
@@ -4,117 +4,92 @@ var mqtt = require('mqtt')
 var fs = require('fs');
 var SensorTag = require('sensortag');
 
-var cooldown = false;
-var timer = false;
-var temperatureTimer = false;
+const SENSOR_NAME = 'Door';
 
-var client = mqtt.connect(process.env.MQTT_HOST, {
+const TEMPERATURE_MEASUREMENT_COOLDOWN = 3000; // Every 3 seconds
+const TEMPERATURE_UPPER_THRESHOLD = 25;
+const TEMPERATURE_LOWER_THRESHOLD = 16;
+const TOO_HOT_COOLDOOWN = 30000; // 30 sec cooldown
+const TOO_HOT_STRING = 'Open the window! It is to hot, hot, hot in here, so take of all your clothes!';
+const TOO_COLD_COOLDOOWN = 30000; // 30 sec cooldown
+const TOO_COLD_STRING = 'Brr! Its cold in here. There must be som Toros in the atmosphere!'
+
+const DO_NOT_SLAM_COOLDOWN = 10000;
+const DO_NOT_SLAM_STRING = 'Do NOT slam the door!';
+
+const publish = (topic, object, noJSON) => {
+  if (!connected) return console.log('Not connected to MQTT yet');
+  client.publish(topic, noJSON ? object : JSON.stringify(object));
+}
+
+/**
+ * TEMPERATURE MEASUREMENTS
+ */
+let lastTempMeasurement = Date.now();
+let lastHotMessage = Date.now();
+let lastColdMessage = Date.now();
+const handleTemperatureChange = (objectTemp, ambientTemp) => {
+  const now = Date.now();
+  if (now - lastTempMeasurement > TEMPERATURE_MEASUREMENT_COOLDOWN) {
+    publish('paradise/api/temperature', { sensor: SENSOR_NAME, temperature: ambientTemp });
+    publish('paradise/log/temperature', { sensor: SENSOR_NAME, objectTemp: objectTemp, ambientTemp: ambientTemp });
+    lastTempChange = now;
+  }
+
+  if (now - lastHotMessage > TOO_HOT_COOLDOOWN && ambientTemp > TEMPERATURE_UPPER_THRESHOLD) {
+    publish('paradise/notify/tts', TOO_HOT_STRING, true);
+    lastHotMessage = now;
+  }
+
+  else if (now - lastColdMessage > TOO_COLD_COOLDOOWN && ambientTemp < TEMPERATURE_LOWER_THRESHOLD) {
+    publish('paradise/notify/tts', TOO_COLD_STRING, true);
+    lastColdMessage = now;
+  }
+}
+
+/**
+ * DOOR SLAMMING MEASUREMENTS
+ */
+let lastSlamMessage = Date.now();
+const handleAccelerometerChange = (x, y, z) => {
+  const now = Date.now();
+  if (now - lastSlamMessage > DO_NOT_SLAM_COOLDOWN) {
+    var xAcc = x.toFixed(1);
+    var yAcc = y.toFixed(1);
+    var zAcc = z.toFixed(1);
+    if(Math.pow(Math.pow(yAcc,2)+Math.pow(zAcc,2),0.5) > 0.75) {
+      publish('paradise/notify/tts', DO_NOT_SLAM_STRING);
+    }
+  }
+}
+
+// Setup MQTT
+const client = mqtt.connect(process.env.MQTT_HOST, {
   cert: fs.readFileSync('ca.crt'),
   rejectUnauthorized: false
 });
 
+let connected = false;
 client.on('connect', function () {
-	console.log('Connected to MQTT')
-})
+	console.log('Connected to MQTT');
+  connected = true;
+});
 
-var log = function(text) {
-  if(text) {
-    console.log(text);
-  }
-}
-
-//==============================================================================
-// Step 1: Connect to sensortag device.
-//------------------------------------------------------------------------------
-// It's address is printed on the inside of the red sleeve
-// (replace the one below).
+// Setup SensorTag
 var ADDRESS = "BC:6A:29:26:8C:B1";
+SensorTag.discoverByAddress(ADDRESS, (tag) => {
+  console.log('Discovered SensorTag');
+  tag.connectAndSetup(() => {
+    console.log('SensorTag connected and set up');
 
-var connected = new Promise((resolve, reject) => SensorTag.discoverByAddress(ADDRESS, (tag) => resolve(tag)))
-  .then((tag) => new Promise((resolve, reject) => tag.connectAndSetup(() => resolve(tag))));
+    tag.enableIrTemperature();
+    tag.setIrTemperaturePeriod(300);
+    tag.notifyIrTemperature();
 
-console.log(ADDRESS)
-//==============================================================================
-// Step 2: Enable the sensors you need.
-//------------------------------------------------------------------------------
-// For a list of available sensors, and other functions,
-// see https://github.com/sandeepmistry/node-sensortag.
-// For each sensor enable it and activate notifications.
-// Remember that the tag object must be returned to be able to call then on the
-// sensor and register listeners.
-var sensor = connected.then(function(tag) {
-  log("connected");
+    tag.enableAccelerometer();
+    tag.notifyAccelerometer();
 
-  tag.enableIrTemperature(log);
-  tag.setIrTemperaturePeriod(300,log);
-  tag.notifyIrTemperature(log);
-
-  tag.enableAccelerometer(log);
-  tag.notifyAccelerometer(log);
-
-  return tag;
-});
-
-//==============================================================================
-// Step 3: Register listeners on the sensor.
-//------------------------------------------------------------------------------
-// You can register multiple listeners per sensor.
-//
-
-
-// A simple example of an act on the irTemperature sensor.
-sensor.then(function(tag) {
-  tag.on("irTemperatureChange", function(objectTemp, ambientTemp) {
-    if (!timer) {
-      client.publish('paradise/log/temperature', "Temperature: " + ambientTemp.toString());
-	var json ='{"temperature":'+ambientTemp.toString() +'"sensor": "SensorTag" }'
-
-	var json = {
-		"sensor": "SensorTag",
-		"temperature": ambientTemp.toString()
-	}
-	console.log(json);
-      client.publish('paradise/api/temperature', JSON.stringify(json));
-      console.log("Temperature: " + ambientTemp)
-      if(ambientTemp > 25 && !temperatureTimer) {
-          client.publish('paradise/notify/temperature', 'Open the window! It is to hot, hot, hot in here, so take of all your clothes!');
-          setTimeout(function() { temperatureTimer = false; }, 100000);
-          temperatureTimer = true;
-          console.log("Inne i too hot")
-      }
-      setTimeout(function() { timer = false; }, 3000);
-      timer = true;
-
-	}
-  })
-});
-
-// Accelerometer test
-sensor.then(function(tag) {
-	console.log("I accelerometer")
-	tag.on("accelerometerChange", function(x, y, z) {
-	var xAcc = x.toFixed(1);
-        var yAcc = y.toFixed(1);
-        var zAcc = z.toFixed(1);
-	if(Math.pow(Math.pow(yAcc,2)+Math.pow(zAcc,2),0.5)> 0.75 ) {
-
-		if (!cooldown) {
-			console.log("Door slammed too hard!")
-			client.publish('paradise/notify/door-slam', 'Do NOT slam the door!')
-			setTimeout(function() { cooldown = false; }, 10000);
-			cooldown = true;
-		}
-	}
+    tag.on('irTemperatureChange', handleTemperatureChange);
+    tag.on('accelerometerChange', handleAccelerometerChange);
   });
-});
-
-
-
-
-//==============================================================================
-// Step 4 (optional): Configure periods for sensor reads.
-//------------------------------------------------------------------------------
-// The registered listeners will be invoked with the specified interval.
-sensor.then(function(tag) {
-  tag.setIrTemperaturePeriod(3000, log);
 });
